23b923fbf56618f1b4937090220d8b5c0debbd0b
Christian Baker <cwbaker4@gmail.com>
2016-01-31 18:43:48 -0500

    Added a typing animation to the header

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+
+$(document).ready(function() { 
+
+console.log("Am I working?")
+
+$(function(){
+      $(".tag_line").typed({
+        strings: ["Introducing a New Way To Find Your Skis", "And Without Looking Like a Gaper"],
+        typeSpeed: 0
+      });
+  });
+
+// $(".navigation_bar li").hover(function() { //This adds css background color when you hover over the h3 element
+//       $(this).css("background-color", "yellow");
+// });
+
+  });
+
+
+   // $("h1").hide(); // Hides the element. Can also do show().
+   //  $("h1").fadeIn(5000); // the 5000 describes how long it will take to finish the animation in milliseconds. Note: you can only use animate with CSS properties that take in a numeric value, so couldn't do like color with this. Right now I'm shrinking the width of this thing from 500px to 100px over 5 seconds.
+   //  $(".color_block").animate({ width:100 }, 5000, function() {
+   //      alert("animation finished!");
+   //  }); 
+
+   //  $("h2").click(function() { // This calls the function only after the element was clicked
+   //      $("h2").fadeOut(5000);
+   //    });
+   //  $("h3").hover(function() { //This adds css background color when you hover over the h3 element
+   //    $(this).css("background-color", "yellow");
+   //  });
+   //  $(".picture").click(function() { // will add a border upon a click
+   //    $(this).css("border", "5px solid black");
+   //    console.log("You clearly want to play basketball");
+   //  });
+   //  $("h4").hover(function() { // will make the font bigger if you hover over it
+   //    $(this).css("font-size", "100px");
+   //  });
